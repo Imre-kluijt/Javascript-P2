ca5043016c098070f249f2029418af29359f6898
I. Kluijt <9029448@student.zadkine.nl>
2025-12-04 13:50:13 +0100

    opdracht 5

---
+++ Opdracht 5/script.js
@@ -0,0 +1,7 @@
+let text= "20";
+let x= 10;
+let z= x + x;
+let getal_z= Number(text) + x;
+
+document.getElementById("getal").innerHTML = "Getal opgeteld: " + z;
+document.getElementById("getalstring").innerHTML = "Getal en string opgeteld: " + getal_z;
