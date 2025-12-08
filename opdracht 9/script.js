c35302b35e5b125ac6779a4b68b6f3c777bcb906
I. Kluijt <9029448@student.zadkine.nl>
2025-12-08 13:53:15 +0100

    opdracht 9

---
+++ opdracht 9/script.js
@@ -0,0 +1,8 @@
+function verberg_tekst() {
+    document.getElementById("tekst").style.display = "none";
+}
+
+function toon_tekst() {
+    document.getElementById("tekst").style.display = "block";
+    document.getElementById("tekst").style.backgroundColor = "lightyellow";
+}
