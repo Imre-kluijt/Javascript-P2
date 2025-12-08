3ceb7fa8cc162ff134dc92297122179370df6e1f
I. Kluijt <9029448@student.zadkine.nl>
2025-12-08 13:44:23 +0100

    opdracht 8

---
+++ opdracht 8/script.js
@@ -0,0 +1,7 @@
+let lightbtn = document.getElementById("aanpasbtn");
+
+function kleur_aanpassen() {
+    document.getElementById("tekst").style.color = "red";
+    document.getElementById("tekst").innerText = "inhoud aangepast!";
+}
+
